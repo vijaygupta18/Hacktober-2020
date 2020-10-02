f98be9b43dd12557e9077ac3e6bd19275ac28226
Anders Kok <ak.kornerup@gmail.com>
2020-10-02 16:58:23 +0200

    Added vue todo list project

---
+++ vue-todo-list/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Todo list</h1>
+  <TodoList />
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import TodoList from './components/TodoList.vue';
+
+@Options({
+  components: {
+    TodoList,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-todo-list/src/components/TodoList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="todo-list">
+    <div class="input">
+      <input type="text" v-model="input" />
+      <button @click="addTask">Add</button>
+    </div>
+		<br>
+		<hr>
+    <ul>
+      <li v-for="task in tasks" :key="task">{{ task }}</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({})
+export default class TodoList extends Vue {
+  tasks: string[] = [];
+  input = "";
+
+  addTask() {
+    if(!this.input) return;
+
+    this.tasks.push(this.input);
+    this.input = "";
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.todo-list {
+  width: 200px;
+	margin: 0px auto;
+}
+</style>
