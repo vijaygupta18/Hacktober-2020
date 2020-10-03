fd2e87cb3d3f10c36f18a73a66083ff05d1d9291
gitanshwadhwa28 <55893529+gitanshwadhwa28@users.noreply.github.com>
2020-10-04 03:43:22 +0530

    Add files via upload

---
+++ fitness-website-prototype/js/script.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+ $(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        }
+    });
+  });
