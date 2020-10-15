2ce91a2977fdf0330ee1bce722834bf2faee1a55
ronniepratap <72915027+ronniepratap@users.noreply.github.com>
2020-10-15 14:48:44 +0530

    Create FLOW.py

---
+++ Python programs/FLOW.py
@@ -0,0 +1,25 @@
+# cook your dish here
+
+# cook your dish here
+
+t=int(input())
+
+for x in range(t):
+
+    a,b,c=input().split()
+
+    a=int(a)
+
+    b=int(b)
+
+    c=int(c)
+
+    d=180
+
+    if(a+b+c==d):
+
+        print("YES")
+
+    else:
+
+        print("NO")
