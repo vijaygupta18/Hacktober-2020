1f6edc1c9e84a6ea4240f3869872de265a9e9d81
codarc264 <ashutoshbshaha30@gmail.com>
2020-10-15 13:12:04 +0530

    Web Based Chat App

---
+++ chat-app/src/index.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+let count = 0
+
+io.on('connection', (socket) => {
+    console.log('New Websocket Connection')
+
+    socket.emit('countUpdated')
+})
+
+server.listen(port, () => {
+    console.log('Server is up on port '+port+'!')
+})
+
