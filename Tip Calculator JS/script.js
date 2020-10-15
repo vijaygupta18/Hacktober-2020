54061002800549c68ab5b285a732dc9bfe64d065
Annisa Purbandari <a.purbandari@gmail.com>
2020-10-15 21:46:24 +1100

    added split bill

--- Tip Calculator JS/script.js
+++ Tip Calculator JS/script.js
@@ -1,6 +1,7 @@
 var tipEl = document.querySelector("#tip-percentage");
 var totalEl = document.querySelector("#total");
 var submitEl = document.querySelector("#submit");
+var splitEl = document.querySelector("#split");
 
 function calculateTip(total, tipPercentage) {
   var roundedResult = (total * tipPercentage).toFixed(2);
@@ -21,4 +22,15 @@ function addTip(event) {
   document.querySelector("#new-total").textContent = newTotal.toFixed(2);
 }
 
+function splitTotal(event) {
+  event.preventDefault();
+
+  var total = document.querySelector("#new-total").textContent;
+  var numPeople = document.querySelector("#num-people").value;
+
+  var newTotal = (total / numPeople).toFixed(2);
+  document.querySelector("#split-total").textContent = newTotal;
+}
+
 submitEl.addEventListener("click", addTip);
+splitEl.addEventListener("click", splitTotal);
